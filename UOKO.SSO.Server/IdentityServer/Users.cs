bb73ce30bf5c34c834cad73904ec53511a081917
qingpan <qingpan@uoko.com>
2015-11-16 14:27:12 +0800

    增加democontroller

--- UOKO.SSO.Server/IdentityServer/Users.cs
+++ UOKO.SSO.Server/IdentityServer/Users.cs
@@ -1,8 +1,11 @@
 ﻿using IdentityServer3.Core;
 using IdentityServer3.Core.Services.InMemory;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,28 @@ namespace SSO.Domain.IdentityServer
     {
         public static List<InMemoryUser> Get()
         {
+            //var inMemoryUserData = new List<InMemoryUser>();
+            //var userData = GetUserAll();
+            //foreach (var item in userData)
+            //{
+            //    inMemoryUserData.Add(new InMemoryUser()
+            //    {
+            //        Username = item.LoginName,
+            //        Password = item.Password,
+            //        Subject = "1",
+            //        Claims = new[]
+            //        {
+            //            new Claim(Constants.ClaimTypes.GivenName, "Bob"),
+            //            new Claim(Constants.ClaimTypes.FamilyName, "Smith"),
+            //            new Claim(Constants.ClaimTypes.Role, "Geek"),
+            //            new Claim(Constants.ClaimTypes.Role, "Foo")
+            //        }
+
+            //    });
+            //}
+            //return inMemoryUserData;
+
+            #region 静态配置
             return new List<InMemoryUser>
             {
                 new InMemoryUser
@@ -30,6 +55,36 @@ namespace SSO.Domain.IdentityServer
                     }
                 }
             };
+            #endregion
+        }
+        private static string SystemApiUrl
+        {
+            get
+            {
+                var url = ConfigurationManager.AppSettings["system.api.url"];
+                return url;
+            }
+        }
+
+        public class UserInfo
+        {
+            public string LoginName { get; set; }
+            public string Password { get; set; }
+        }
+
+        /// <summary>
+        /// 获取CF_User表的所有用户信息
+        /// </summary>
+        /// <returns></returns>
+        private static List<UserInfo> GetUserAll()
+        {
+            var client = new HttpClient { Timeout = TimeSpan.FromSeconds(5) };
+            var getUserInfoApiUrl = string.Format("{0}/UserOld", SystemApiUrl);
+
+            var result11 = client.GetAsync(getUserInfoApiUrl).Result.Content.ReadAsStringAsync().Result;
+
+            var result = JsonConvert.DeserializeObject<List<UserInfo>>(client.GetAsync(getUserInfoApiUrl).Result.Content.ReadAsStringAsync().Result);
+            return result;
         }
     }
 }
