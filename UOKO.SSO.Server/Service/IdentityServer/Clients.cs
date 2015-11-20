255e862a8a6d45c7fb895c1168be86547e619a1b
$+j <lust4life.jun@gmail.com>
2015-11-20 17:50:57 +0800

    add systemset site client

--- UOKO.SSO.Server/Service/IdentityServer/Clients.cs
+++ UOKO.SSO.Server/Service/IdentityServer/Clients.cs
@@ -8,50 +8,69 @@ namespace UOKO.SSO.Server.Service.IdentityServer
         public static IEnumerable<Client> Get()
         {
             return new[]
-            {
-                new Client
-                {
-                    
-                    ClientName = "波多系统",
-                    ClientId = "etadmin",
-                    Flow = Flows.Implicit,
-                    RequireConsent=false,
-                    RedirectUris = new List<string>
-                    {
-                        "http://etadmin.uoko.ioc/",
-                        "http://sso.one.com/",
-                        "http://two.domain.com/",
-                        "http://sso.three.com/",
-                    },
+                   {
+                       new Client
+                       {
 
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://sso.domain.com/",
-                        "http://sso.one.com/",
-                        "http://two.domain.com/",
-                        "http://sso.three.com/",
-                    },
-                    
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "profile",
-                        "roles",
-                        "sampleApi"
-                    },
-                },
-                new Client
-                {
-                    ClientName = "系统配置权限系统",
-                    ClientId = "systemset",
-                    Flow = Flows.ClientCredentials,
+                           ClientName = "波多系统",
+                           ClientId = "etadmin",
+                           Flow = Flows.AuthorizationCode,
+                           RequireConsent = false,
+                           RedirectUris = new List<string>
+                                          {
+                                              "http://etadmin.uoko.ioc/",
+                                              "http://sso.one.com/",
+                                              "http://two.domain.com/",
+                                              "http://sso.three.com/",
+                                          },
 
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret-for-systemset".Sha256())
-                    }
-                }
-            };
+                           PostLogoutRedirectUris = new List<string>
+                                                    {
+                                                        "http://sso.domain.com/",
+                                                        "http://sso.one.com/",
+                                                        "http://two.domain.com/",
+                                                        "http://sso.three.com/",
+                                                    },
+
+                           AllowedScopes = new List<string>
+                                           {
+                                               "openid",
+                                               "profile",
+                                               "roles",
+                                               "sampleApi"
+                                           },
+                       },
+
+
+                       new Client
+                       {
+                           ClientName = "系统配置权限系统",
+                           ClientId = "systemset",
+                           Flow = Flows.Hybrid,
+                           RequireConsent = false,
+                           RedirectUris = new List<string>()
+                                          {
+                                              "http://mgmt.test.uoko.ioc/",
+                                          },
+                           AllowedScopes = new List<string>
+                                           {
+                                               "openid",
+                                               "profile",
+                                           },
+                       },
+
+                       new Client
+                       {
+                           ClientName = "系统配置权限系统-api",
+                           ClientId = "systemset-api",
+                           Flow = Flows.ClientCredentials,
+
+                           ClientSecrets = new List<Secret>
+                                           {
+                                               new Secret("secret-for-systemset-api".Sha256())
+                                           }
+                       }
+                   };
         }
     }
 }
