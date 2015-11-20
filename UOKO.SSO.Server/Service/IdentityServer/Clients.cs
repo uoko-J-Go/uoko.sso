b92d418291b50675c793c6c18e94e57b47dc5c54
zhiping <zhiping@uoko.com>
2015-11-20 11:46:54 +0800

    修正Merge错误

--- UOKO.SSO.Server/Service/IdentityServer/Clients.cs
+++ UOKO.SSO.Server/Service/IdentityServer/Clients.cs
@@ -10,60 +10,7 @@ namespace UOKO.SSO.Server.Service.IdentityServer
         private static string configPath = HttpContext.Current.Server.MapPath("~/Configs/IdentityServer/ClientsConfig.json");
         public static IEnumerable<Client> Get()
         {
-<<<<<<< HEAD
             return JsonConfigHelper<List<Client>>.Load(configPath);
-=======
-            return new[]
-            {
-                new Client
-                {
-                    
-                    ClientName = "波多系统",
-                    ClientId = "etadmin",
-                    Flow = Flows.Implicit,
-                    RequireConsent=false,
-                    RedirectUris = new List<string>
-                    {
-                        "http://etadmin.uoko.ioc/",
-                        "http://sso.one.ioc/",
-                        "http://two.uoko.ioc/",
-                        "http://sso.three.ioc/",
-                    },
-
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://sso.uoko.ioc/",
-                        "http://sso.one.ioc/",
-                        "http://two.uoko.ioc/",
-                        "http://sso.three.ioc/",
-                        "http://etadmin.uoko.ioc/",
-                    },
-                    
-                    AllowedScopes = new List<string>
-                    {
-                        "openid",
-                        "profile",
-                        "roles",
-                        "sampleApi"
-                    },
-                },
-                new Client
-                {
-                    ClientName = "MVC Client (service communication)",
-                    ClientId = "mvc_service",
-                    Flow = Flows.ClientCredentials,
-
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        "sampleApi"
-                    }
-                }
-            };
->>>>>>> b600961349faf2d743e6d5fe8c38db198d9a9712
         }
     }
 }
