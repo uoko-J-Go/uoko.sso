f8fc6df9558baf76946b192540735c629a2763e7
$+j <lust4life.jun@gmail.com>
2015-11-23 18:16:11 +0800

    临时使用 client scope 方便测试

--- UOKO.SSO.Server/Service/IdentityServer/Clients.cs
+++ UOKO.SSO.Server/Service/IdentityServer/Clients.cs
@@ -10,71 +10,74 @@ namespace UOKO.SSO.Server.Service.IdentityServer
         private static string configPath = HttpContext.Current.Server.MapPath("~/Configs/IdentityServer/ClientsConfig.json");
         public static IEnumerable<Client> Get()
         {
-//            return new[]
-//                   {
-//                       new Client
-//                       {
+            // todo: after-remove 调试方便, 先不走配置
+            return new[]
+                   {
+                       new Client
+                       {
 
-//                           ClientName = "波多系统",
-//                           ClientId = "etadmin",
-//                           Flow = Flows.AuthorizationCode,
-//                           RequireConsent = false,
-//                           RedirectUris = new List<string>
-//                                          {
-//                                              "http://etadmin.uoko.ioc/",
-//                                              "http://sso.one.com/",
-//                                              "http://two.domain.com/",
-//                                              "http://sso.three.com/",
-//                                          },
+                           ClientName = "波多系统",
+                           ClientId = "etadmin",
+                           Flow = Flows.AuthorizationCode,
+                           RequireConsent = false,
+                           RedirectUris = new List<string>
+                                          {
+                                              "http://etadmin.uoko.ioc/",
+                                              "http://sso.one.com/",
+                                              "http://two.domain.com/",
+                                              "http://sso.three.com/",
+                                          },
 
-//                           PostLogoutRedirectUris = new List<string>
-//                                                    {
-//                                                        "http://sso.domain.com/",
-//                                                        "http://sso.one.com/",
-//                                                        "http://two.domain.com/",
-//                                                        "http://sso.three.com/",
-//                                                    },
+                           PostLogoutRedirectUris = new List<string>
+                                                    {
+                                                        "http://sso.domain.com/",
+                                                        "http://sso.one.com/",
+                                                        "http://two.domain.com/",
+                                                        "http://sso.three.com/",
+                                                    },
 
-//                           AllowedScopes = new List<string>
-//                                           {
-//                                               "openid",
-//                                               "profile",
-//                                               "roles",
-//                                               "sampleApi"
-//                                           },
-//                       },
+                           AllowedScopes = new List<string>
+                                           {
+                                               "openid",
+                                               "profile",
+                                               "roles",
+                                               "sampleApi"
+                                           },
+                       },
 
 
-//                       new Client
-//                       {
-//                           ClientName = "系统配置权限系统",
-//                           ClientId = "systemset",
-//                           Flow = Flows.Hybrid,
-//                           RequireConsent = false,
-//                           RedirectUris = new List<string>()
-//                                          {
-//                                              "http://mgmt.test.uoko.ioc/",
-//                                          },
-//                           AllowedScopes = new List<string>
-//                                           {
-//                                               "openid",
-//                                               "profile",
-//                                           },
-//                       },
+                       new Client
+                       {
+                           ClientName = "系统配置权限系统",
+                           ClientId = "systemset",
+                           Flow = Flows.Hybrid,
+                           RequireConsent = false,
+                           RedirectUris = new List<string>()
+                                          {
+                                              "http://mgmt.test.uoko.ioc/",
+                                          },
+                           AllowedScopes = new List<string>
+                                           {
+                                               "openid",
+                                               "profile",
+                                           },
+                       },
 
-//                       new Client
-//                       {
-//                           ClientName = "系统配置权限系统-api",
-//                           ClientId = "systemset-api",
-//                           Flow = Flows.ClientCredentials,
+                       new Client
+                       {
+                           ClientName = "系统配置权限系统-api",
+                           ClientId = "systemset-api",
+                           Flow = Flows.ClientCredentials,
 
-//                           ClientSecrets = new List<Secret>
-//                                           {
-//                                               new Secret("secret-for-systemset-api".Sha256())
-//                                           }
-//                       }
-//                   };
-            return JsonConfigHelper<List<Client>>.Load(configPath);
+                           ClientSecrets = new List<Secret>
+                                           {
+                                               new Secret("secret-for-systemset-api".Sha256())
+                                           }
+                       }
+                   };
+
+            var clients = JsonConfigHelper<List<Client>>.Load(configPath);
+            return clients;
         }
     }
 }
--- UOKO.SSO.Server/Service/IdentityServer/Scopes.cs
+++ UOKO.SSO.Server/Service/IdentityServer/Scopes.cs
@@ -10,31 +10,33 @@ namespace UOKO.SSO.Server.Service.IdentityServer
         private static string configPath = HttpContext.Current.Server.MapPath("~/Configs/IdentityServer/ScopesConfig.json");
         public static IEnumerable<Scope> Get()
         {
-            //var scopes = new List<Scope>
-            //             {
-            //                 new Scope
-            //                 {
-            //                     Enabled = true,
-            //                     Name = "roles",
-            //                     Type = ScopeType.Identity,
-            //                     Claims = new List<ScopeClaim>
-            //                              {
-            //                                  new ScopeClaim("role")
-            //                              }
-            //                 },
-
-            //                 new Scope
-            //                 {
-            //                     Name = "systemset-api",
-            //                     DisplayName = "系统配置权限系统-api",
-            //                     Description = "Access to a sample API",
-            //                     Type = ScopeType.Resource,
-            //                 }
-            //             };
-
             var scopes = JsonConfigHelper<List<Scope>>.Load(configPath);
+
+            // todo: after-remove 调试方便, 先不走配置
+            scopes = new List<Scope>
+                         {
+                             new Scope
+                             {
+                                 Enabled = true,
+                                 Name = "roles",
+                                 Type = ScopeType.Identity,
+                                 Claims = new List<ScopeClaim>
+                                          {
+                                              new ScopeClaim("role")
+                                          }
+                             },
+
+                             new Scope
+                             {
+                                 Name = "systemset-api",
+                                 DisplayName = "系统配置权限系统-api",
+                                 Description = "Access to a sample API",
+                                 Type = ScopeType.Resource,
+                             }
+                         };
+
             scopes.AddRange(StandardScopes.All);
             return scopes;
-        }
+        }   
     }
 }
