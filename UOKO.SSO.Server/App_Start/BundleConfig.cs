76dc223150c379c88a59fb30839aaefa31eb31f4
zhiping <zhiping@uoko.com>
2015-11-25 11:02:29 +0800

    小调整

--- UOKO.SSO.Server/App_Start/BundleConfig.cs
+++ UOKO.SSO.Server/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace UOKO.SSO.Server
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/app.css"));
+                      "~/Content/site.css"));
         }
     }
 }
