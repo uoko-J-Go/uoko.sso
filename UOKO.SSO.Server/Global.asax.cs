d4abbbca5f316b3553816eefe4a400f208f7ef21
$+j <lust4life.jun@gmail.com>
2015-12-01 17:06:37 +0800

    remove old sso

--- UOKO.SSO.Server/Global.asax.cs
+++ UOKO.SSO.Server/Global.asax.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Security.Claims;
 using System.Threading;
 using System.Web;
-using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using NLog.Config;
-using UOKO.SSO.Core;
-using UOKO.SSO.Server.Utils;
 
 namespace UOKO.SSO.Server
 {
@@ -24,38 +18,6 @@ namespace UOKO.SSO.Server
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-
-
-        public override void Init()
-        {
-            base.Init();
-
-            //this.AuthenticateRequest += OnAuthenticateRequest;
-        }
-
-        /// <summary>
-        /// 在这里构造身份信息
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="eventArgs"></param>
-        private void OnAuthenticateRequest(object sender, EventArgs eventArgs)
-        {
-            var cookieInfo = SSOAuthentication.GetAuthCookieInfo(ServerConfig.CookieName);
-
-            if (cookieInfo != null
-                && !string.IsNullOrWhiteSpace(cookieInfo.Alias))
-            {
-                // 有身份信息,构建基础身份信息
-                var userInfo = SSOAuthentication.GenerateClaimsPrincipal(cookieInfo);
-
-                if (this.Context != null)
-                {
-                    this.Context.User = userInfo;
-                }
-
-                Thread.CurrentPrincipal = userInfo;
-            }
         }
     }
 }
